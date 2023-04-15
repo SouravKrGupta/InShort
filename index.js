dec229ff07e0350f01d77461470cf65d2b43cb11
Sourav Kumar Gupta <89971045+SouravKrGupta@users.noreply.github.com>
2023-04-15 17:17:24 +0530

    update index

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(cors());
 app.use("/", route);
 
 //static file
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const PORT = 8000;
